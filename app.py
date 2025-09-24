4b1583fa4405c5f258367d461fd38632bc667cd7
tinngo777 <tinpro777@gmail.com>
2025-09-24 01:14:50 -0500

    update gitignore and audioplayer

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import streamlit as st
 from core import handle_file_upload, run_audio_pipeline
-from ui import show_title_and_uploader, show_results
+from ui import show_title_and_uploader, show_results, show_audio_controls
 
 def main():
     uploaded_file, file_path = show_title_and_uploader()
-
     if uploaded_file:
+        input_audio_path = handle_file_upload(uploaded_file, file_path)
+        show_audio_controls(input_audio_path)
         if st.button("Separate Audio"):
             with st.spinner("Separating audio, please wait."):
-                input_audio_path = handle_file_upload(uploaded_file, file_path)
                 stem_folder, zip_path = run_audio_pipeline(input_audio_path, uploaded_file.name)
-
                 st.session_state.stem_folder = stem_folder
                 st.session_state.zip_path = zip_path
                 show_results(stem_folder, zip_path)
